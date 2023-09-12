4e129e89b353ed750d723a10b5e973a7e1973bcb
Adithya <adithya3012@gmail.com>
2023-09-12 12:59:25 +0530

    Removed unnecessary import in backend

--- node/app.js
+++ node/app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const userRouter = require("./routes/userRouter");
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-app.use("/api/login", userRouter);
